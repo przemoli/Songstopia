02046836b797fbf219179d2f0ebedfe65ac52942
Przemysław Lib <przemyslaw.lib@gmail.com>
2015-11-18 18:40:55 +0100

    Configure FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+
             new AppBundle\AppBundle(),
         );
 
