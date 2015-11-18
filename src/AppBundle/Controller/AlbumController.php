9b33c2e10d44eacfc44b2fe0c12971565fe45f36
Przemysław Lib <przemyslaw.lib@gmail.com>
2015-11-18 20:31:02 +0100

    Add apidoc annotation for each rest action

--- src/AppBundle/Controller/AlbumController.php
+++ src/AppBundle/Controller/AlbumController.php
@@ -4,12 +4,34 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * @RouteResource("Album")
  */
 class AlbumController extends FOSRestController
 {
+  /**
+   * @ApiDoc()
+   */
   public function getAction($bandID, $id)
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function postAction()
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function putAction()
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function deleteAction($id)
   {}
 }
--- src/AppBundle/Controller/BallotController.php
+++ src/AppBundle/Controller/BallotController.php
@@ -4,12 +4,34 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * @RouteResource("Ballot")
  */
 class BallotController extends FOSRestController
 {
+  /**
+   * @ApiDoc()
+   */
   public function getAction($id)
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function postAction()
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function putAction()
+  {}
+
+  /**
+   * @ApiDoc()
+   */
+  public function deleteAction($id)
   {}
 }
